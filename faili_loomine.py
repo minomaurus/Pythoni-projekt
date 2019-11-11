6002f679ee6fbddce485d2f4c039e7cdf52f8600
dragonflyzz <57600718+dragonflyzz@users.noreply.github.com>
2019-11-11 20:47:03 +0200

    Add files via upload

---
+++ faili_loomine.py
@@ -0,0 +1,18 @@
+fail= input("Sisestage tekstifaili nimi: ")
+from random import randint
+import re
+f= open(fail)
+sisu= f.read() #loeb sisu
+lause= sisu.split(".") #teeb sisu lausete listiks
+f.close()
+def tee_lünktekstiks(tekst):
+    tekst_listina=tekst.split() #teeb teksti listiks sõnadest
+    lünk=tekst_listina[randint(1,len(tekst_listina))-1] #võtab juhusliku järjenumbriga sõna listist
+    lünk=lünk.strip().strip(',') #eemaldab kirjavahemärgid, mis võivad lause sees olla
+    lünktekst=tekst.replace(lünk,'_'*len(lünk))
+    return lünktekst
+l= open("lünktekst.txt", "w")
+for i in lause:
+    laused= str(i)
+    l.write(tee_lünktekstiks(laused))
+l.close()
